f10a1524ad654ae9d9d81ee12617bfb1a9b3dc26
Yeşim Gökmen <yesimgokmen24@gmail.com>
2021-01-17 21:18:58 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle( Musteri musteri)
+        {
+           Console.WriteLine("Müşteri Eklendi"+musteri.MusteriId+ " *"+musteri.MusteriAdi+ " *" + musteri.MusteriSoyadi+ " *"+musteri.MusterininYasi);
+
+
+        }
+        
+        public void Sil(Musteri Musteri) 
+        {
+
+            Console.WriteLine("Müşteri Silindi");
+        
+        }
+
+        public void Listele(Musteri MusteriListesi) 
+        {
+
+            Console.WriteLine( "Müşterilerin listesi :::::"+MusteriListesi.MusteriId +" *"+MusteriListesi.MusteriAdi+"*"+MusteriListesi.MusteriSoyadi+"*"+ MusteriListesi.MusterininYasi);
+        
+        }
+        
+
+       
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri MusteriBilgisi1 = new Musteri();
+
+            MusteriBilgisi1.MusteriId = 123456789;
+            MusteriBilgisi1.MusteriAdi = "Metin";
+            MusteriBilgisi1.MusteriSoyadi = "M:O";
+            MusteriBilgisi1.MusterininYasi = 26;
+
+            Musteri MusteriBilgisi2 = new Musteri();
+
+            MusteriBilgisi2.MusteriId = 985467;
+            MusteriBilgisi2.MusteriAdi = "Dikdörtgen";
+            MusteriBilgisi2.MusteriSoyadi = "L";
+            MusteriBilgisi2.MusterininYasi = 60;
+
+            Musteri MusteriBilgisi3 = new Musteri();
+
+            MusteriBilgisi3.MusteriId = 325463245;
+            MusteriBilgisi3.MusteriAdi = "Kare";
+            MusteriBilgisi3.MusteriSoyadi = "K";
+            MusteriBilgisi3.MusterininYasi = 28;
+
+
+            
+            Musteri[] Musteriler = new Musteri[] { MusteriBilgisi1, MusteriBilgisi2, MusteriBilgisi3 };
+
+            
+            foreach (Musteri MusteriListesi in Musteriler)
+            {
+                Console.WriteLine(MusteriListesi.MusteriId);
+                Console.WriteLine(MusteriListesi.MusteriAdi);
+                Console.WriteLine(MusteriListesi.MusteriSoyadi);
+                Console.WriteLine(MusteriListesi.MusterininYasi);
+                Console.WriteLine("********");
+            }
+          
+            Console.WriteLine("---------------------------------");
+
+            MusteriManager musterimanager = new MusteriManager();
+            musterimanager.Ekle(MusteriBilgisi1);
+            musterimanager.Ekle(MusteriBilgisi2);
+            musterimanager.Ekle(MusteriBilgisi3);
+
+            Console.WriteLine("///////////////////////////////////////////////////////");
+
+            MusteriManager musteriimanager = new MusteriManager();
+
+            musteriimanager.Sil(MusteriBilgisi1);
+            musteriimanager.Sil(MusteriBilgisi2);
+            musteriimanager.Sil(MusteriBilgisi3);
+
+            Console.WriteLine("///////////////////////////////////////////////////////");
+
+
+            MusteriManager musteriimanageri = new MusteriManager();
+            musteriimanageri.Listele(MusteriBilgisi1);
+            musteriimanageri.Listele(MusteriBilgisi2);
+            musteriimanageri.Listele(MusteriBilgisi3);
+
+
+
+        }
+    }
+}
